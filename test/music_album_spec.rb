4f9c898564731d06d6e6f23aee808f6619d67140
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-19 16:37:16 -0600

    Refactor test cases for accepting 1 argument with all atributes within

--- test/music_album_spec.rb
+++ test/music_album_spec.rb
@@ -3,20 +3,23 @@ require 'date'
 
 describe MusicAlbum do
   before :each do
-    @genre = 'Rock'
-    @author = 'Metallica'
-    @source = 'Internet Stream'
-    @label = 'Best Exits'
-    @publish_date = Date.new(2023, 9, 19)
-    @on_spotify = false
-    @album = MusicAlbum.new(@genre, @author, @source, @label, @publish_date, @on_spotify)
+    @album_attributes = {
+      genre: 'Rock',
+      author: 'Metallica',
+      source: 'Internet Stream',
+      label: 'Best Exits',
+      publish_date: Date.new(2023, 9, 19),
+      on_spotify: false
+    }
+
+    @album = MusicAlbum.new(@album_attributes)
   end
   it 'Creates a MusicAlbum instance with the correct attributes' do
-    expect(@album.genre).to eql(@genre)
-    expect(@album.author).to eql(@author)
-    expect(@album.source).to eql(@source)
-    expect(@album.label).to eql(@label)
-    expect(@album.publish_date).to eql(@publish_date)
-    expect(@album.on_spotify).to eql(@on_spotify)
+    expect(@album.genre).to eql(@album_attributes[:genre])
+    expect(@album.author).to eql(@album_attributes[:author])
+    expect(@album.source).to eql(@album_attributes[:source])
+    expect(@album.label).to eql(@album_attributes[:label])
+    expect(@album.publish_date).to eql(@album_attributes[:publish_date])
+    expect(@album.on_spotify).to eql(@album_attributes[:on_spotify])
   end
 end

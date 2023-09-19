8d582ebabc31d4e77c2ca317339618ddf1f3c3f2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-18 22:32:00 -0600

    Add music_album_spec.rb and include 2 tests

---
+++ test/music_album_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../src/music_album'
+require_relative '../src/item'
+=begin
+All MusicAlbum class properties visible in the diagram should
+ be defined and set up in the constructor method.
+=end
+  
+describe MusicAlbum do
+  before :each do
+    @album = MusicAlbum.new(
+      "Rock",
+      "Metallica",
+      "Internet",
+      "Best one",
+      "1990-12-25",
+      false
+    )
+  end
+
+  it 'Should return true if parent\' method returns true AND if on_spotify 
+  equals true ' do 
+    allow(@album).to receive(:on_spotify) {true}
+    expect(@album.can_be_archived?).to eql(true)
+  end
+
+  it 'Should return false if parent method is true AND if on_spotify is false' do
+    allow(@album).to receive(:on_spotify) {false}
+    expect(@album.can_be_archived?).to eql(false)
+  end
+end
+

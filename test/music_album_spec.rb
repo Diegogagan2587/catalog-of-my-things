579c4c03109f3c21172fd78f1d292cba252dce42
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-19 14:24:07 -0600

    refactor test

--- test/music_album_spec.rb
+++ test/music_album_spec.rb
@@ -1,9 +1,5 @@
 require_relative '../src/music_album'
 require_relative '../src/item'
-=begin
-All MusicAlbum class properties visible in the diagram should
- be defined and set up in the constructor method.
-=end
   
 describe MusicAlbum do
   before :each do
@@ -12,20 +8,20 @@ describe MusicAlbum do
       "Metallica",
       "Internet",
       "Best one",
-      "1990-12-25",
-      archived:false
+      "1990",
+      true,
     )
   end
 
   it 'Should return true if parent\' method .can_be_archived? returns true AND if on_spotify 
   equals true ' do 
     allow(@album).to receive(:on_spotify) {true}
-    expect(@album.can_be_archived?).to eql(true)
+    expect(@album.can_be_archived?).to be_truthy
   end
 
   it 'Should return false if parent method .can_be_archived? is true AND if on_spotify is false' do
     allow(@album).to receive(:on_spotify) {false}
-    expect(@album.can_be_archived?).to eql(false)
+    expect(@album.can_be_archived?).to be_falsy
   end
 end
 

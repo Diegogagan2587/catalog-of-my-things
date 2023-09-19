2d169c346df8d6a9e0e8c368cd1f498fd94e701c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-19 13:02:59 -0300

    Add option select logc

--- src/app.rb
+++ src/app.rb
@@ -9,6 +9,32 @@ class App
   end
 
   def option_select
+    option = gets.chomp.to_i
+    case option
+    when 1
+      list_books
+    when 2
+      list_music_albums
+    when 3
+      list_games
+    when 4
+      list_genres
+    when 5
+      list_labels
+    when 6
+      list_authors
+    when 7
+      add_book
+    when 8
+      add_music_album
+    when 9
+      add_game
+    when 10
+      puts 'Thank you for using this app!'
+      exit
+    else
+      puts 'Invalid option, try again.'
+    end
   end
 
   def run

b04ed7a197bff9ce84ce0c82222ac24686dd1511
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-09-20 23:35:34 +0200

    Add initialize

--- src/game.rb
+++ src/game.rb
@@ -4,4 +4,13 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
+  def initialize(options = {})
+    super(options[:genre],
+      options[:author],
+      options[:label],
+      options[:publish_date])
+    @multiplayer = options[:multiplayer]
+    @last_played_at = Date.parse options[:last_played_at]
+  end
+
 end

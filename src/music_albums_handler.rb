b8974e323308674dbefd990ce986b386a5bf20de
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-20 16:11:09 -0600

    Fix linters erros

--- src/music_albums_handler.rb
+++ src/music_albums_handler.rb
@@ -4,8 +4,9 @@ class MusicAlbumHandler
   end
 
   def list_all_music_albums(albums)
-    albums.each{ |album |
-      next unless album.class == MusicAlbum
+    albums.each do |album|
+      next unless album.instance_of?(MusicAlbum)
+
       id = album.id
       genre = album.genre
       author = album.author
@@ -14,15 +15,15 @@ class MusicAlbumHandler
       publish_date = album.publish_date
       on_spotify = album.on_spotify
       puts [
-        "",
+        '',
         "Id: #{id}",
         "Genre: #{genre}",
         "Author: #{author}",
         "Source: #{src}",
         "Label: #{label}",
         "Publish_date: #{publish_date}",
-        "Is on spotify?: #{on_spotify}",
+        "Is on spotify?: #{on_spotify}"
       ]
-    }
+    end
   end
 end
--- test/music_albums_handler_spec.rb
+++ test/music_albums_handler_spec.rb
@@ -5,7 +5,7 @@ require_relative '../src/app'
 
 describe MusicAlbumHandler do
   before :each do
-    #thinsg will be done before each test
+    # thinsg will be done before each test
     @album_attributes = {
       genre: 'Rock',
       author: 'Metallica',
@@ -28,24 +28,24 @@ describe MusicAlbumHandler do
   it 'Should List all music albums' do
     music_album_1 = MusicAlbum.new(@album_attributes)
     music_album_2 = MusicAlbum.new(@album_attributes)
-    allow(music_album_1).to receive(:id) {1}
-    allow(music_album_2).to receive(:id) {2}
+    allow(music_album_1).to receive(:id) { 1 }
+    allow(music_album_2).to receive(:id) { 2 }
     items = [music_album_1, music_album_2]
     expected_output = /Id:\s*1\s*Genre:\s*Rock\s*Author:\s*Metallica\s*Source:\s*Internet Stream\s*Label:\s*Best Exits\s*Publish_date:\s*2023-09-19\s*Is on spotify\?:\s*false\s*Id:\s*2\s*Genre:\s*Rock\s*Author:\s*Metallica\s*Source:\s*Internet Stream\s*Label:\s*Best Exits\s*Publish_date:\s*2023-09-19\s*Is on spotify\?:\s*false/
-    expect {@music_album_handler.list_all_music_albums(items)}.to output(
+    expect { @music_album_handler.list_all_music_albums(items) }.to output(
       expected_output
     ).to_stdout
   end
 
-  it "should only display music albums" do
+  it 'should only display music albums' do
     music_album = MusicAlbum.new(@album_attributes)
-    other_item = Item.new('x','x','x','x','x')
-    allow(music_album).to receive(:id) {1}
-    allow(other_item).to receive(:id) {2}
+    other_item = Item.new('x', 'x', 'x', 'x', 'x')
+    allow(music_album).to receive(:id) { 1 }
+    allow(other_item).to receive(:id) { 2 }
     items = [music_album, other_item]
     expected_output = /Id:\s*1\s*Genre:\s*Rock\s*Author:\s*Metallica\s*Source:\s*Internet Stream\s*Label:\s*Best Exits\s*Publish_date:\s*2023-09-19\s*Is on spotify\?:\s*false\s*/
-    
-    expect {@music_album_handler.list_all_music_albums(items)}.to output(
+
+    expect { @music_album_handler.list_all_music_albums(items) }.to output(
       expected_output
     ).to_stdout
   end

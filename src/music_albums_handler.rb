a7c4f9290aaaa7f4145d9b5f12c0aa9b81f1d3b7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-21 02:41:40 -0600

    Implement method for adding music albums

--- src/music_albums_handler.rb
+++ src/music_albums_handler.rb
@@ -1,6 +1,53 @@
-class MusicAlbumHandler
-  def add_item(item, store)
-    store.push(item)
+require_relative 'music_album'
+require_relative 'genre'
+require_relative 'genre_handler'
+
+module MusicAlbumHandler
+  include GenreHandler
+  def create_music_album
+    puts ['Select a Gender by number or add one,']
+    list_genres(@genres)
+    print 'Genre: '
+    genre_input = gets.chomp
+    puts genre_input    
+    genre = if genre_input.match?(/\A-?\d+\z/)
+      @genres[genre_input.to_i]
+    else
+      Genre.new(genre_input)
+    end
+    
+    #@genres[genre_input.to_i] || Genre.new(genre_input)
+  
+    print 'Author: '
+    author = gets.chomp
+    print 'Source: '
+    src = gets.chomp
+    print 'Label: '
+    label = gets.chomp
+    print 'Publish date: '
+    publish_date = gets.chomp
+    print ['Is on spotify ? (type true or false)']
+    spotify_input = gets.chomp.strip.downcase
+    on_spotify = spotify_input == 'true'
+
+    album_data = {
+      genre: genre,
+      author: author,
+      source: src,
+      label: label,
+      publish_date: publish_date,
+      on_spotify: on_spotify
+    }
+    music_album = MusicAlbum.new(album_data)
+    genre.add_item(music_album)
+    return music_album
+  end
+
+  def add_music_album_to(items,gender)
+    music_album = create_music_album
+    items.push(music_album)
+    gender.push(music_album.genre) unless gender.include?(music_album.genre)
+    puts ['','Music album added succsesfully']
   end
 
   def list_all_music_albums(albums)
@@ -8,7 +55,7 @@ class MusicAlbumHandler
       next unless album.instance_of?(MusicAlbum)
 
       id = album.id
-      genre = album.genre
+      genre = album.genre.name
       author = album.author
       src = album.source
       label = album.label

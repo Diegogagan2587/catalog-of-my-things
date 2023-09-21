639a1cadfa0ea1a27759e65b49e9607c5b562433
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-21 15:20:05 -0600

    Fix Error: saving data outside root folder, include method wrapper write_genres

--- src/store/write.rb
+++ src/store/write.rb
@@ -3,8 +3,8 @@ require 'fileutils'
 
 class Write
   def create_file(file_name)
-    FileUtils.mkdir_p('../data') unless File.directory?('./data')
-    FileUtils.touch("../data/#{file_name}.json") unless File.exist?("../data/#{file_name}.json")
+    FileUtils.mkdir_p('./data') unless File.directory?('./data')
+    FileUtils.touch("./data/#{file_name}.json") unless File.exist?("./data/#{file_name}.json")
   end
 
   def store(albums, genres)
@@ -12,11 +12,15 @@ class Write
     write_genres(genres)
   end
 
+  def write_genres(genres)
+    puts 'Saving genres into storage...! '
+  end
+
   def write_music_albums(albums)
     puts 'Saving music albums into storage...! '
     data_to_store = []
     albums.each do |album|
-      next unless (album.class = MusicAlbum)
+      next unless (album.class == MusicAlbum)
 
       data_to_store.push({
                            'genre' => album.genre,
@@ -31,6 +35,6 @@ class Write
     return if data_to_store.empty?
 
     create_file('music_albums')
-    File.write('../data/music_albums.json', JSON.pretty_generate(data_to_store))
+    File.write('./data/music_albums.json', JSON.pretty_generate(data_to_store))
   end
 end

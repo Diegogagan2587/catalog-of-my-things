8db5403ad119175de75742c016ec4cbe64105a36
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-21 15:08:14 -0600

    Fix Error on  write_music_albums: not saving data

--- src/store/write.rb
+++ src/store/write.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 class Write
   def create_file(file_name)
-    FileUtils.mkdir_p('../data') unless File.directory?("./data")
+    FileUtils.mkdir_p('../data') unless File.directory?('./data')
     FileUtils.touch("../data/#{file_name}.json") unless File.exist?("../data/#{file_name}.json")
   end
 
@@ -15,19 +15,22 @@ class Write
   def write_music_albums(albums)
     puts 'Saving music albums into storage...! '
     data_to_store = []
-    albums.each do | album |
-      next unless album.class = MusicAlbum
+    albums.each do |album|
+      next unless (album.class = MusicAlbum)
+
       data_to_store.push({
-        'genre' => album.genre,
-        'author' => album.author,
-        'source' => album.source,
-        'label' => album.label,
-        'publish_date' => album.publish_date,
-        'on_spotify' => album.on_spotify,
-      })
-      #preven saving if no data 
-      return if data_to_store.empty?
-      create_file('music_albums')
-      File.write('../data/music_albums.json')
+                           'genre' => album.genre,
+                           'author' => album.author,
+                           'source' => album.source,
+                           'label' => album.label,
+                           'publish_date' => album.publish_date,
+                           'on_spotify' => album.on_spotify
+                         })
+    end
+    # preven saving if no data
+    return if data_to_store.empty?
+
+    create_file('music_albums')
+    File.write('../data/music_albums.json', JSON.pretty_generate(data_to_store))
   end
 end

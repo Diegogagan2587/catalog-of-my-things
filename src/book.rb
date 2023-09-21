cb3c656c306b6e1192eaceb4bc179c5d5beda114
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-21 11:11:05 -0300

    Add to_h method to book.rb

--- src/book.rb
+++ src/book.rb
@@ -12,6 +12,18 @@ class Book < Item
     @cover_state = params[:cover_state]
   end
 
+  def to_h
+    {
+      id: @id,
+      genre: @genre,
+      author: @author,
+      source: @source,
+      label: @label.to_h,
+      publish_date: @publish_date,
+      cover_state: @cover_state
+    }
+  end
+
   private
 
   def can_be_archived?

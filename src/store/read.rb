83704a4dcba58ef3fc01a45300cd399133d6a8c8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-21 16:11:07 -0600

    Implemment restore_genres method

--- src/store/read.rb
+++ src/store/read.rb
@@ -1,3 +1,18 @@
+require_relative '../genre'
+
 class Read
-  def 
+  #then restore genres
+  def and_restore_genres(state)
+    return unless File.exist?('./data/genres.json')
+    store = File.open('./data/genres.json')
+    genres_data = JSON.parse(store.read)
+    genres_data.each { | genre |
+      state.push( Genre.new(genre.name) )
+    }
+    store.close
+  end
+  #then restore albums
+  def and_restore_albums()
+    puts 'Restoring'
+  end
 end

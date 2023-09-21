888dfcbae50d839445ce0afdd24ae4e66dee2abd
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-09-22 01:51:50 +0200

    Update game_methods.rb

--- src/game_methods.rb
+++ src/game_methods.rb
@@ -3,4 +3,7 @@ require_relative 'author'
 require_relative 'game'
 
 module PreservegameLabel
+  DATA_DIR = 'data'.freeze
+  GAME_FILE = File.join(DATA_DIR, 'game.json')
+  AUTHOR_FILE = File.join(DATA_DIR, 'author.json')
 end
